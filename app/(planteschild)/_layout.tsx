6e563b7db69a3516d5797599bb8f96928174d023
0usko <ouskozn@gmail.com>
2024-02-06 18:10:45 +0100

    change layout

--- app/(planteschild)/_layout.tsx
+++ app/(planteschild)/_layout.tsx
@@ -1,6 +1,9 @@
 import { Link, Stack, Tabs } from 'expo-router';
 import { Pressable, useColorScheme, Animated } from 'react-native';
 import AddButton from '../../components/AddButton';
+import { Component } from 'react';
+import PlantesChildsScreen from './plantschilds';
+import { AntDesign, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export const unstable_settings = {
 
@@ -13,14 +16,35 @@ export default function Layout() {
   return (
     <Stack
           screenOptions={{
-              title: "Fleurs & Plantes ornementales",
+              title: "",
               headerRight: () => (
                   <Pressable>
                     <AddButton path={'/addplants'}/>
                   </Pressable>
-              ),
-                  
-          }} />
+              ),          
+          } }>
+    <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({  }) => <FontAwesome name="home" color='blue' />,
+        }}
+      />
+      <Tabs.Screen
+        name="plante"
+        options={{
+          title: 'Plantes',
+          tabBarIcon: ({ }) => <MaterialCommunityIcons name="flower-outline" color='green' />,
+        }}
+      />
+       <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <AntDesign name="profile" color={color} />,
+        }}
+      />
+    </Stack>
     
   );
 }
