162264f76ee9996d6934d55ff996cb6a5471ee14
Aurel <aurelien010603@gmail.com>
2024-02-27 19:48:06 +0100

    Users fetch finished (missing token by local storage)

--- app/fetchAPI/users.tsx
+++ app/fetchAPI/users.tsx
@@ -1,4 +1,6 @@
-export default async function getUsers() {
+//ToDo Replace personal token by the variable in the AsyncStorage (not here for now)
+
+export default async function getUser() {
     var myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer 1|uWKAFpgmX5yOpganFyQyOK7HHjCXVRPzfGybOAtp82701a14");
 
@@ -12,12 +14,90 @@ export default async function getUsers() {
         .catch(error => console.log('error', error));
 }
 
-export async function getUser(id: { toString: () => string; }) {
+export async function updateUser(username, email, password, phone, first_name, last_name, profile_picture, bio){
+    const requestOptions = {
+        method: "PUT",
+        headers: {
+            "Authorization": "Bearer Bearer 1|uWKAFpgmX5yOpganFyQyOK7HHjCXVRPzfGybOAtp82701a14",
+            "username": username,
+            "email": email,
+            "password": password,
+            "phone": phone,
+            "first_name": first_name,
+            "last_name": last_name,
+            "profile_picture": profile_picture,
+            "bio": bio
+        }
+    };
+
+    fetch("https://leafylinks.maxim-le-cookie.fr/api/users/me", requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+}
+
+export async function deleteUser(){
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer Bearer 1|uWKAFpgmX5yOpganFyQyOK7HHjCXVRPzfGybOAtp82701a14");
+
+    const requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+    };
+
+    fetch("https://leafylinks.maxim-le-cookie.fr/api/users/me", requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+}
+
+export async function getUserById(id: { toString: () => string; }) {
     var requestOptions = {
         method: 'GET',
     };
 
     return await fetch("https://leafylinks.maxim-le-cookie.fr/api/users/" + id, requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+}
+
+export async function register(username, email, password){
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            'username' : username,
+            'email' : email,
+            'password' : password,
+        }
+    };
+
+    return await fetch("https://leafylinks.maxim-le-cookie.fr/api/users/register", requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+}
+
+export async function login(email, password){
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            'email' : email,
+            'password' : password,
+        }
+    };
+
+    return await fetch("https://leafylinks.maxim-le-cookie.fr/api/users/login", requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+}
+
+export async function logout(){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer 1|uWKAFpgmX5yOpganFyQyOK7HHjCXVRPzfGybOAtp82701a14");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+    };
+
+    return await fetch("https://leafylinks.maxim-le-cookie.fr/api/users/logout", requestOptions)
         .then(response => response.json())
         .catch(error => console.log('error', error));
 }
